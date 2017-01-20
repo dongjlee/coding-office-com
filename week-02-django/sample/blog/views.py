c944236d7fd00670beb35d59e6649675b6bc9691
dongjlee <djdjexp@gmail.com>
2017-01-20 18:00:18 +0900

    week2 until model

--- week-02-django/sample/blog/views.py
+++ week-02-django/sample/blog/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+# from random import randint
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You`re at the index")
+    # ramdom_number = randint(1, 10)
+    # return HttpResponse("Hello, world. {}".format(ramdom_number) )
+    # return HttpResponse("Hello, world. You`re at the index")
+    name = "dong ju"
+    return render(request, "index.html", { "name" : name })
